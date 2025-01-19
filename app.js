131f6f4f83ced3c9901d0775ba3ade34282b9352
cs.odcaceres <cs.odcaceres@gmail.com>
2025-01-19 17:43:24 -0300

    Modificacion archivo app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Variables
+//const inputAmigo = document.getElementById("amigo");
 // Array para almacenar los nombres
 let listaNombres = [];
 
@@ -12,26 +13,48 @@ function asignarTextoElemento(elemento, texto){
 }
 
 function agregarAmigo() {
-    let amigo = document.querySelector('#amigo').value;
+    let nombreAmigo = document.querySelector('#amigo').value;
     
-    console.log(amigo);
+    console.log(nombreAmigo);
     
-    if (amigo == null || amigo == ''){
+    if (nombreAmigo == null || nombreAmigo == ''){
 
-        limpiarCaja();
+        alert("Por favor, ingresa un nombre válido.");
 
     } else {
 
-        listaNombres.push(amigo);
+        listaNombres.push(nombreAmigo);
         limpiarCaja();
-        asignarTextoElemento('#listaAmigos', amigo);
+        //asignarTextoElemento('#listaAmigos', nombreAmigo);
+        actualizarListaAmigos();        
         console.log(listaNombres);
 
     }
-    
 } 
 
-// Funcio para limpiar el cuadro input cuando hacemos click en el boton añadir.
+// Funcion que actualiza la lista de amigos para despues utilizara la funcion sortearAmigo()
+function actualizarListaAmigos() {
+    listaAmigos.innerHTML = "";
+    listaNombres.forEach((amigo) => {
+      const li = document.createElement("li");
+      li.textContent = amigo;
+      listaAmigos.appendChild(li);
+    });
+}
+
+// donde se realiza un sorteo aleatorio de la lista listaNombres
+function sortearAmigo() {
+    if (listaNombres.length === 0) {
+        alert("La lista está vacía. Por favor, agregar nombres a la lista primero.");
+        return;
+      }
+
+      const indiceAleatorio = Math.floor(Math.random() * listaNombres.length);
+      const amigoSorteado = listaNombres[indiceAleatorio];
+      resultado.innerHTML = `El amigo secreto es: <strong>${amigoSorteado}</strong>`;
+}
+
+// Funcion para limpiar el cuadro input cuando hacemos click en el boton añadir.
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
